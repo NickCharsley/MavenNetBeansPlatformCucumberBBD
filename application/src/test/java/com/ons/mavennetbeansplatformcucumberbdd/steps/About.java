40910d0f170bbc65522e59fec9b72d3d4d1f870f
Nick Charsley <nick@hotBlack>
2015-09-15 08:09:24 +0100

    Removed slowness in detecting 'closed'/ non-existent dialogues. By setting DialogWaiter.WaitDialogTimeout to 1 second for detecting if Dialog has not been closed.

--- application/src/test/java/com/ons/mavennetbeansplatformcucumberbdd/steps/About.java
+++ application/src/test/java/com/ons/mavennetbeansplatformcucumberbdd/steps/About.java
@@ -6,10 +6,15 @@
 package com.ons.mavennetbeansplatformcucumberbdd.steps;
 
 import cucumber.api.java.en.*;
+import java.io.IOException;
 import static org.hamcrest.CoreMatchers.*;
 import static org.junit.Assert.*;
 import org.netbeans.jellytools.NbDialogOperator;
 import org.netbeans.jellytools.actions.ActionNoBlock;
+import org.netbeans.jemmy.TimeoutExpiredException;
+import org.netbeans.jemmy.Timeouts;
+import org.netbeans.jemmy.operators.ComponentOperator;
+import org.openide.util.Exceptions;
 //import org.netbeans.jellytools.JellyTestCase;
 //import org.netbeans.jellytools.actions.ActionNoBlock;
 
@@ -32,17 +37,6 @@ public class About {
         if (doMe) new ActionNoBlock(menu, null).performMenu();
     }
 
-    @Then("^the \"([^\"]*)\" Dialogue is displayed$")
-    public void theDialogueIsDisplayed(String dialogue) {
-        NbDialogOperator doDialogue=null;
-        try {
-            if (doMe) doDialogue=new NbDialogOperator(dialogue);
-            assertThat("The '"+dialogue+"' Dialogue is displayed",doDialogue,is(notNullValue()));
-        } catch (Exception e) {
-            assertThat("The '"+dialogue+"' Dialogue is displayed",doDialogue,is(notNullValue()));            
-        }
-    }
-
     @When("^I can click the \"([^\"]*)\" close button$")
     public void clickTheCloseButton(String dialogue) {
         // Express the Regexp above with the code you wish you had
@@ -50,15 +44,28 @@ public class About {
         if (doMe) new NbDialogOperator("About").closeByButton();
     }
 
+    static final boolean dialogVisable(String dialog,boolean visable){       
+        NbDialogOperator doDialog=null;
+        Timeouts to=ComponentOperator.getEnvironmentOperator().getTimeouts();        
+        to.setTimeout("DialogWaiter.WaitDialogTimeout", visable?60000:1000);
+        try {
+            doDialog=new NbDialogOperator(dialog);
+        } catch (TimeoutExpiredException e) {
+            //NOP as we are looking for this.
+            //Nothing to see here.
+        } finally {
+            to.setTimeout("DialogWaiter.WaitDialogTimeout", 60000);      
+        }
+        return doDialog!=null;
+    }           
+    
     @Then("^the \"([^\"]*)\" Dialogue is hidden$")
     public void theDialogueIsHidden(String dialogue) {
-        NbDialogOperator doDialogue=null;
-        try {
-            if (doMe) doDialogue=new NbDialogOperator(dialogue);
-            assertThat("The '"+dialogue+"' Dialogue is hidden",doDialogue,is(nullValue()));
-        } catch (Exception e) {
-            assertThat("The '"+dialogue+"' Dialogue is hidden",doDialogue,is(nullValue()));
-        }
+        assertThat("The '"+dialogue+"' Dialogue is hidden",dialogVisable(dialogue,false),is(false));
     }    
 
+    @Then("^the \"([^\"]*)\" Dialogue is displayed$")
+    public void theDialogueIsDisplayed(String dialogue) {
+        assertThat("The '"+dialogue+"' Dialogue is displayed",dialogVisable(dialogue,true),is(true));
+    }
 }
